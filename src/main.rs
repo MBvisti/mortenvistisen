484eb6c60336b05a6332b3d29de6a6074c11edc4
mbv <mbv@mortenvistisen.com>
2022-06-13 13:36:57 +0200

    chore: setup basic index with tera

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use tera::{Context, Tera};
+
 fn main() {
-    println!("Hello, world!");
+    let tera = match Tera::new("templates/**/*") {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Parsing error(s): {}", e);
+            ::std::process::exit(1);
+        }
+    };
+    let context = Context::new();
+    let r = tera
+        .render("index.html", &context)
+        .expect("expected to be rendered");
+    println!("{}", r)
 }
