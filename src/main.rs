fa723aea77334432eb71150be5cf46c87b030a9c
mbv <mbv@mortenvistisen.com>
2022-06-15 19:11:42 +0200

    basic setup with actix done

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,42 @@
+use actix_web::{http::Error, web, App, HttpResponse, HttpServer, error, middleware};
 use tera::{Context, Tera};
 
-fn main() {
-    let tera = match Tera::new("templates/**/*") {
-        Ok(t) => t,
-        Err(e) => {
-            println!("Parsing error(s): {}", e);
-            ::std::process::exit(1);
-        }
-    };
+// store tera template in application state
+async fn index(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
+    println!("hitting index");
     let context = Context::new();
-    let r = tera
-        .render("index.html", &context)
-        .expect("expected to be rendered");
-    println!("{}", r)
+
+    let s = match tmpl
+        .render("index.html", &context) {
+            Ok(s) => s,
+            Err(e) => {
+                println!("{}", e);
+                error::ErrorInternalServerError(e).to_string()
+            },
+        };
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=debug");
+    env_logger::init();
+
+    println!("Listening on: 127.0.0.1:8080, open browser and visit have a try!");
+    HttpServer::new(|| {
+        let tera = match Tera::new("templates/**/*.html") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+        App::new()
+            .app_data(web::Data::new(tera))
+            .wrap(middleware::Logger::default()) // enable logger
+            .service(web::resource("/").route(web::get().to(index)))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
