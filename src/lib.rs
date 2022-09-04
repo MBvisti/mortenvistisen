3dc8fc1444b2d06ab0bb601c969763d645511ca3
MBvisti <mbv@mortenvistisen.com>
2022-09-04 14:59:53 +0200

    chore: add robots.txt and adjust for lighthouse suggestions

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
 use actix_files as fs;
+use fs::NamedFile;
 use handlers::not_found;
-use std::net::TcpListener;
+use std::{net::TcpListener, path::PathBuf};
 
 #[macro_use]
 extern crate lazy_static;
 
-use actix_web::{dev::Server, middleware, web, App, HttpResponse, HttpServer};
+use actix_web::{
+    dev::Server,
+    get,
+    http::header::{ContentDisposition, DispositionType},
+    middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer,
+};
 use tera::Tera;
 
 pub mod handlers;
@@ -24,6 +30,12 @@ lazy_static! {
     };
 }
 
+#[get("/robots.txt")]
+async fn robots_text(_req: HttpRequest) -> Result<fs::NamedFile, Error> {
+    let file = fs::NamedFile::open_async("static/robots.txt").await?;
+    Ok(file.use_last_modified(true))
+}
+
 pub fn start_blog(listener: TcpListener) -> Result<Server, std::io::Error> {
     let tmpl = web::Data::new(TEMPLATES.clone());
     let srv = HttpServer::new(move || {
@@ -31,7 +43,9 @@ pub fn start_blog(listener: TcpListener) -> Result<Server, std::io::Error> {
             .app_data(web::Data::clone(&tmpl))
             .wrap(middleware::Logger::default()) // enable logger
             .route("/status", web::get().to(HttpResponse::Ok))
+            .service(robots_text)
             .service(fs::Files::new("/static", "static/").use_last_modified(true))
+            // .service(fs::Files::new("/static", "static/robots.txt").use_last_modified(true))
             .service(handlers::index)
             .service(handlers::render_post)
             .default_service(web::route().to(not_found))
