02138af7c3d9620806a56878a2dde91a6a91cbb0
MBvisti <mbv@mortenvistisen.com>
2023-12-07 13:11:39 +0100

    moving resource files

--- resources/tailwind/config.js
+++ resources/tailwind/config.js
@@ -1,16 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "views/internal/pages/**/*.templ",
-    "views/internal/layouts/**/*.templ",
-    "views/internal/components/**/*.templ",
+    "../../views/internal/pages/**/*.templ",
+    "../../views/internal/layouts/**/*.templ",
+    "../../views/internal/components/**/*.templ",
     "node_modules/preline/dist/*.js",
   ],
   theme: {
     extend: {},
-  },
-  daisyui: {
-    themes: ["dark"],
   },
   plugins: [
     require('preline/plugin'),
