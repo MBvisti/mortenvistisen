d1e8ef10935c46783726f95b4de6e10f599cb66b
MBvisti <mbv@mortenvistisen.com>
2024-01-18 18:49:11 +0100

    wip

--- views/article_templ.go
+++ views/article_templ.go
@@ -34,14 +34,14 @@ func unsafe(html string) templ.Component {
 
 func renderSubscribeForm(csrfToken, title string) templ.ComponentScript {
 	return templ.ComponentScript{
-		Name: `__templ_renderSubscribeForm_dec3`,
-		Function: `function __templ_renderSubscribeForm_dec3(csrfToken, title){const targetDiv = document.querySelector("#subscribe-form");
+		Name: `__templ_renderSubscribeForm_eccf`,
+		Function: `function __templ_renderSubscribeForm_eccf(csrfToken, title){const targetDiv = document.querySelector("#subscribe-form");
 
 	targetDiv.innerHTML = ` + "`" + `
 		<div class="max-w-xl text-center mx-auto">
 			<div class="mb-5">
-				<h3 class="text-2xl font-bold md:text-3xl md:leading-tight text-white">Get infrequent updates about new posts</h3>
-				<p class="text-base">I try to (unsuccessfully) release a new article once a month.</p>
+				<h3 class="text-2xl font-bold md:text-3xl md:leading-tight text-white">Monthly technical deep dives.</h3>
+				<p class="text-base">I work with everything from backend dev, systems designs to devops and machine learning. I share my learnings twice a month in tutorial from, so you can pick up new concept faster and expand your technical tool belt.</p>
   			</div>
   			<form hx-post="/subscribe" hx-target="this" hx-swap="outerHTML" method="POST" action="/subscribe" >
 				<input type="hidden" name="gorilla.csrf.Token" value="${csrfToken}"/>
@@ -49,19 +49,19 @@ func renderSubscribeForm(csrfToken, title string) templ.ComponentScript {
 					<input type="hidden" name="article-title" value="${title}"/>
 					<input required type="email" id="hero-input" name="hero-input" class="py-3 px-4 block w-full border-gray-200 rounded-lg 
   				          text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none 
-  				          bg-slate-900 border-gray-700 text-gray-400 focus:ring-gray-600" placeholder="Enter your email" />
+  				          bg-slate-900 border-gray-700 text-gray-400 focus:ring-gray-600" placeholder="deep-dives@mortenvistisen.com" />
   				      <button data-umami-event="newsletter--${title}" type="submit" class="w-full sm:w-auto whitespace-nowrap py-3 px-4 inline-flex justify-center 
   				          items-center gap-x-2 text-sm font-semibold rounded-lg border bg-slate-600 
   				          text-white hover:bg-slate-900 disabled:opacity-50 disabled:pointer-events-none focus:outline-none 
   				          focus:ring-1 focus:ring-gray-600">
-  				        Subscribe
+  				        Receive Tutorials
   				      </button>
   				</div>
 			</form>
   		</div>
   ` + "`" + `;}`,
-		Call:       templ.SafeScript(`__templ_renderSubscribeForm_dec3`, csrfToken, title),
-		CallInline: templ.SafeScriptInline(`__templ_renderSubscribeForm_dec3`, csrfToken, title),
+		Call:       templ.SafeScript(`__templ_renderSubscribeForm_eccf`, csrfToken, title),
+		CallInline: templ.SafeScriptInline(`__templ_renderSubscribeForm_eccf`, csrfToken, title),
 	}
 }
 
