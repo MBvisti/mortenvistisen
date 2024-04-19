6b6cff03faa75fa4a68edc33c8ef37b697adbdea
MBvisti <mbv@mortenvistisen.com>
2024-04-19 14:52:02 +0200

    chore: revamp dashboard

--- controllers/dashboard.go
+++ controllers/dashboard.go
@@ -27,10 +27,77 @@ func (c *Controller) DashboardSubscribers(ctx echo.Context) error {
 			ID:           sub.ID.String(),
 			Verified:     sub.IsVerified.Bool,
 			SubscribedAt: sub.SubscribedAt.Time.String(),
+			Refererer:    sub.Referer.String,
 		})
 	}
 
 	return dashboard.Subscribers(viewData, csrf.Token(ctx.Request())).
+		Render(views.ExtractRenderDeps(ctx))
+}
+
+func (c *Controller) DashboardArticles(ctx echo.Context) error {
+	articles, err := c.db.QueryAllPosts(ctx.Request().Context())
+	if err != nil {
+		return err
+	}
+
+	viewData := make([]dashboard.ArticleViewData, 0, len(articles))
+	for _, article := range articles {
+		viewData = append(viewData, dashboard.ArticleViewData{
+			ID:         article.ID.String(),
+			Title:      article.Title,
+			Draft:      article.Draft,
+			ReleasedAt: article.ReleasedAt.Time.String(),
+			Slug:       article.Slug,
+		})
+	}
+
+	return dashboard.Articles(viewData, csrf.Token(ctx.Request())).
+		Render(views.ExtractRenderDeps(ctx))
+}
+
+func (c *Controller) DashboardArticleEdit(ctx echo.Context) error {
+	slug := ctx.Param("slug")
+
+	post, err := c.db.GetPostBySlug(ctx.Request().Context(), slug)
+	if err != nil {
+		return err
+	}
+
+	postContent, err := c.postManager.Parse(post.Filename)
+	if err != nil {
+		return err
+	}
+
+	tags, err := c.db.GetTagsForPost(ctx.Request().Context(), post.ID)
+	if err != nil {
+		return err
+	}
+
+	var keywords string
+	for i, kw := range tags {
+		if i == len(tags)-1 {
+			keywords = keywords + kw.Name
+		} else {
+			keywords = keywords + kw.Name + ", "
+		}
+	}
+
+	return dashboard.ArticleEdit(dashboard.ArticleEditViewData{
+		ID:          post.ID.String(),
+		CreatedAt:   post.CreatedAt.Time,
+		UpdatedAt:   post.UpdatedAt.Time,
+		Title:       post.Title,
+		HeaderTitle: post.HeaderTitle.String,
+		Filename:    post.Filename,
+		Slug:        post.Slug,
+		Excerpt:     post.Excerpt,
+		Draft:       post.Draft,
+		ReleasedAt:  post.ReleasedAt.Time,
+		ReadTime:    post.ReadTime.Int32,
+		Content:     postContent,
+		Keywords:    keywords,
+	}, csrf.Token(ctx.Request())).
 		Render(views.ExtractRenderDeps(ctx))
 }
 
--- repository/database/queries/posts.sql
+++ repository/database/queries/posts.sql
@@ -56,3 +56,19 @@ WHERE
 ORDER BY
     random()
 limit 5;
+
+-- name: QueryAllPosts :many
+SELECT
+    posts.id,
+    posts.created_at,
+    posts.updated_at,
+    posts.title,
+    posts.header_title,
+    posts.filename,
+    posts.slug,
+    posts.excerpt,
+    posts.draft,
+    posts.released_at,
+    posts.read_time
+FROM
+    posts;
