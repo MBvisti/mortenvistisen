65187fc4e8055402724f77a2878feef3afea0782
MBvisti <mbv@mortenvistisen.com>
2024-04-22 20:00:41 +0200

    chore: only provide access to dashboard as admin

--- routes/routes.go
+++ routes/routes.go
@@ -146,7 +146,7 @@ func miscRoutes(router *echo.Echo) {
 }
 
 func dashboardRoutes(router *echo.Echo, ctrl controllers.Controller, mw middleware.Middleware) {
-	dashboardRouter := router.Group("/dashboard", mw.AuthOnly)
+	dashboardRouter := router.Group("/dashboard", mw.AuthOnly, mw.AdminOnly)
 
 	dashboardRouter.GET("", func(c echo.Context) error {
 		return ctrl.DashboardIndex(c)
