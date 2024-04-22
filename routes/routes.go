49744d1b36fedb16e20cc447ae88838d5660b97f
MBvisti <mbv@mortenvistisen.com>
2024-04-22 18:43:41 +0200

    wip

--- routes/routes.go
+++ routes/routes.go
@@ -146,8 +146,7 @@ func miscRoutes(router *echo.Echo) {
 }
 
 func dashboardRoutes(router *echo.Echo, ctrl controllers.Controller, mw middleware.Middleware) {
-	// dashboardRouter := router.Group("/dashboard", mw.AuthOnly)
-	dashboardRouter := router.Group("/dashboard")
+	dashboardRouter := router.Group("/dashboard", mw.AuthOnly)
 
 	dashboardRouter.GET("", func(c echo.Context) error {
 		return ctrl.DashboardIndex(c)
