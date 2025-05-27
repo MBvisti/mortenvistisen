288a0658c061c0ce9d95b81d32d963662548d9cd
mbv <github@mbvlabs.com>
2025-05-27 17:44:19 +0200

    wip

--- assets/js/easymde.js
+++ assets/js/easymde.js
@@ -1,6 +1,27 @@
 import "./easymde-2_26_0.min.js";
 
 (function() {
-	new window.EasyMDE({ element: document.getElementById('my-text-area') })
+	const element = document.getElementById('my-text-area');
+	if (element) {
+		new window.EasyMDE({
+			element: element,
+			placeholder: "Write your article content in Markdown...",
+			spellChecker: false,
+			hideIcons: ["guide", "fullscreen", "side-by-side"],
+			showIcons: ["code", "table"],
+			status: ["autosave", "lines", "words", "cursor"],
+			tabSize: 4,
+			indentWithTabs: false,
+			lineWrapping: true,
+			autofocus: false,
+			toolbar: [
+				"bold", "italic", "strikethrough", "|",
+				"heading-1", "heading-2", "heading-3", "|",
+				"quote", "unordered-list", "ordered-list", "|",
+				"link", "image", "code", "table", "|",
+				"preview", "guide"
+			]
+		});
+	}
 })()
 
