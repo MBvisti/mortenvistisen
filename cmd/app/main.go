b3a65d0692662d4c8265f228395c1af1c606dfdb
mbv <github@mbvlabs.com>
2025-05-28 18:23:53 +0200

    run migrate when deploying to while in progress

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"context"
+	"errors"
+	"flag"
 	"fmt"
+	"io/fs"
 	"log/slog"
 	"os"
 	"os/signal"
+	"strconv"
+	"time"
 
+	"github.com/jackc/pgx/v5/stdlib"
 	"github.com/mbvisti/mortenvistisen/clients"
 	"github.com/mbvisti/mortenvistisen/config"
 	"github.com/mbvisti/mortenvistisen/handlers"
@@ -17,16 +23,134 @@ import (
 	"github.com/mbvisti/mortenvistisen/router"
 	"github.com/mbvisti/mortenvistisen/server"
 	"github.com/mbvisti/mortenvistisen/telemetry"
+	"github.com/pressly/goose/v3"
+	"github.com/pressly/goose/v3/lock"
 	"riverqueue.com/riverui"
 )
 
 var appVersion string
+
+func migrate(ctx context.Context) error {
+	ctx, cancel := context.WithTimeoutCause(
+		ctx,
+		5*time.Minute,
+		errors.New("migration timeout of 5 minutes reached"),
+	)
+	defer cancel()
+
+	cfg := config.NewConfig()
+
+	gooseLock, err := lock.NewPostgresSessionLocker()
+	if err != nil {
+		return err
+	}
+
+	fsys, err := fs.Sub(psql.Migrations, "migrations")
+	if err != nil {
+		return err
+	}
+
+	pool, err := psql.CreatePooledConnection(ctx, cfg.GetDatabaseURL())
+	if err != nil {
+		return err
+	}
+	defer pool.Close()
+
+	db := stdlib.OpenDBFromPool(pool)
+
+	gooseProvider, err := goose.NewProvider(
+		goose.DialectPostgres,
+		db,
+		fsys,
+		goose.WithVerbose(true),
+		goose.WithSessionLocker(gooseLock),
+	)
+	if err != nil {
+		return err
+	}
+
+	ops := flag.String("cmd", "", "")
+	version := flag.String("version", "", "")
+	flag.Parse()
+
+	if *version != "" {
+		v, err := strconv.Atoi(*version)
+		if err != nil {
+			return err
+		}
+
+		switch *ops {
+		case "up":
+			_, err = gooseProvider.UpTo(ctx, int64(v))
+			if err != nil {
+				return err
+			}
+		case "down":
+			_, err = gooseProvider.DownTo(ctx, int64(v))
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	if *version == "" {
+		switch *ops {
+		case "up":
+			_, err = gooseProvider.Up(ctx)
+			if err != nil {
+				return err
+			}
+		case "down":
+			_, err = gooseProvider.Down(ctx)
+			if err != nil {
+				return err
+			}
+		case "upbyone":
+			_, err = gooseProvider.UpByOne(ctx)
+			if err != nil {
+				return err
+			}
+		case "reset":
+			_, err = gooseProvider.DownTo(ctx, 0)
+			if err != nil {
+				return err
+			}
+		case "status":
+			statuses, err := gooseProvider.Status(ctx)
+			if err != nil {
+				return err
+			}
+
+			for _, status := range statuses {
+				slog.Info(
+					"database status",
+					"version",
+					status.Source.Version,
+					"file_name",
+					status.Source.Path,
+					"state",
+					status.State,
+					"applied_at",
+					status.AppliedAt,
+				)
+			}
+		}
+	}
+
+	return nil
+}
 
 func run(ctx context.Context) error {
 	cfg := config.NewConfig()
 
 	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
 	defer cancel()
+
+	if cfg.Environment == config.PROD_ENVIRONMENT {
+		if err := migrate(ctx); err != nil {
+			panic(err)
+		}
+	}
 
 	tel, err := telemetry.New(
 		ctx,
